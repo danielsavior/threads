252f44c2571f9d51ba3862ce02cdb973d96c0774
Daniel Souza da Silva <dssilva@padtec.com.br>
2017-02-06 15:21:52 -0200

    first
    Change-Id: I9be2f4c260126a18a12c6830204cc97f38540822

---
+++ src/br/com/threads/Principal.java
@@ -0,0 +1,9 @@
+package br.com.threads;
+
+public class Principal {
+	
+	public static void main(String[] args) throws InterruptedException {
+        System.out.println("Thread MAIN");
+        Thread.sleep(50000);
+    }
+}
